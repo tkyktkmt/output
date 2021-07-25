c0a8431688ed33363fe2dc48930f494b68752a46
tkyktkmt <tkyktkmt@gmail.com>
2021-07-25 20:34:56 +0900

    度数分布

--- ruby/inject_度数分布.rb
+++ ruby/inject_度数分布.rb
@@ -1,1 +1,7 @@
+data = [:A, :B, :A, :C, :E, :A, :D, :B, :B, :C, :E]
+h = Hash.new(0)
+data.each {|key| h[key] += 1 }
+puts h # => {:A=>3, :B=>3, :C=>2, :E=>2, :D=>1}
 
+data = [:A, :B, :A, :C, :E, :A, :D, :B, :B, :C, :E]
+puts data.inject(Hash.new(0)) {|h, key| h[key] += 1; h }
